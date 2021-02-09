7ec1761e766ff596475b269276bd340c9974e69c
Steven Firth <s.k.firth@lboro.ac.uk>
2021-02-09 09:07:27 +0000

    Updates

--- sap2012/SAP_tables/__init__.py
+++ sap2012/SAP_tables/__init__.py
@@ -4,4 +4,3 @@ from .temperature_reduction_when_heating_is_off_table_9b import temperature_redu
 from .utilisation_factor_for_heating_table_9a import utilisation_factor_for_heating_table_9a
 from .heating_requirement_table_9c import heating_requirement_table_9c
 from .utilisation_factor_for_heating_whole_house_table_9a import utilisation_factor_for_heating_whole_house_table_9a
-from .U2 import U2
